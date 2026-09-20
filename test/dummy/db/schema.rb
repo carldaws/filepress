102669c5b0dd9a5ce92cd7d82dd5194aad1a76cb
Carl Dawson <carldawson@hey.com>
2026-09-20 01:11:05 +0100

    Use the oldest supported schema version in the dummy app

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_22_195517) do
+ActiveRecord::Schema[7.0].define(version: 2025_04_22_195517) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "slug"
